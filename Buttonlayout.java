73f5317a84a9f48c12ad2fb1e1487f68aaab7df9
Tsegab Gebregziabher <tg29285@email.vccs.edu>
2017-12-03 13:01:01 -0500

    initial commit

---
+++ Buttonlayout.java
@@ -0,0 +1,63 @@
+import javax.swing.JPanel;
+
+ import javax.swing.JButton;
+
+ import javax.swing.JFrame;
+
+ import java.awt.FlowLayout;
+
+ import java.awt.GridLayout;
+
+ import java.awt.BorderLayout;
+
+ import javax.swing.JTextField;
+
+
+
+
+
+ 
+
+ public class Buttonlayout extends JFrame {
+
+ 	public Buttonlayout() {
+
+ 		JPanel panel = new JPanel();
+
+ 		panel.setLayout(new FlowLayout());
+
+ 		JTextField [] textarray = new JTextField[4];
+
+ 		for(int i=0; i<textarray.length; i++) {
+
+ 			textarray[i] = new JTextField(10);
+
+ 			panel.add(textarray[i]);
+
+ 		}
+
+ 		//panel.add(textarray[0], BorderLayout.NORTH);
+
+ 		//panel.add(textarray[1], BorderLayout.SOUTH);	
+
+ 		//panel.add(textarray[2], BorderLayout.EAST);
+
+ 		
+
+ 
+
+ 		setSize(200,75);
+
+ 		setTitle("Too many Text Fields O.o");
+
+ 		add(panel);
+
+ 		setVisible(true);
+
+ 	}
+
+ 	public static void main(String[] args) {
+		new Buttonlayout ();
+ 	}
+
+}
